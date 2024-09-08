f9709c427fe6ae0c36da606202ad9e09329e8806
WisenextTime <141509640+WisenextTime@users.noreply.github.com>
2024-09-08 21:24:23 +0800

    daily commit

--- scripts/base/Tile.cs
+++ scripts/base/Tile.cs
@@ -6,9 +6,6 @@ using System.Reflection.Metadata.Ecma335;
 public partial class EditorTile : StaticBody2D
 
 {
-	private string name;
-    public string id;
-    private string type;
     private Sprite2D node_image;
     private NavigationRegion2D node_agent;
     private NavigationObstacle2D node_avoid;
@@ -24,12 +21,18 @@ public partial class EditorTile : StaticBody2D
     }
     private void preload(){
         tile_res ??= GD.Load<TileRes>("res://assests/tiles/TestTile.tres");
-        name=tile_res.Core_name;
-        id=tile_res.Core_id;
-        type=tile_res.Core_type;
         node_image.Texture = tile_res.Image_iamge;
-        node_image.Scale = tile_res.Image_size/16;
-        node_image.Frame=4;
+        switch (tile_res.Core_type){
+            case "ground":
+                node_image.Scale = tile_res.Image_size/16;
+                node_image.Vframes=3;
+                node_image.Hframes=3;
+                node_image.Frame=4;
+                break;
+            default:
+                node_image.Scale = tile_res.Image_size / 16;
+                break;
+        }
         CollisionLayer=tile_res.Layer_physics;
         node_agent.NavigationLayers=tile_res.Layer_agent;
         node_avoid.AvoidanceEnabled=tile_res.Layer_avoid;
--- scripts/base/TileRes.cs
+++ scripts/base/TileRes.cs
@@ -8,11 +8,12 @@ public partial class TileRes : Resource
     [Export] public string Core_id = "";
     [Export] public string Core_name = "";
     [Export] public string Core_type = "ground";
+    [Export] public float Core_Rough = 1f;
     [ExportGroup("image", "Image_")]
     [Export] public Texture2D Image_iamge;
     [Export] public Vector2 Image_size = new(16, 16);
     [ExportGroup("layer", "Layer_")]
     [Export(PropertyHint.Flags,"under,land,low_atitude,high_atitude,SEA,GROUND,AIR,BUILDING")] public uint Layer_physics{get;set;} = 0;
-    [Export(PropertyHint.Flags, "ground,lava,water,waterbridge,ice,cliff_1,cliff_2,cliff_3,cliff_4,void")] public uint Layer_agent { get; set; } = 0;
+    [Export(PropertyHint.Flags, "ground,lava,water,waterbridge,void")] public uint Layer_agent { get; set; } = 0;
     [Export] public bool Layer_avoid;
 }
